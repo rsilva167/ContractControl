fe3ed9b3695bca92fdb7bb4be2dfec40e8d14a75
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-03 21:01:27 -0300

    This is the final version of the program.

---
+++ ContractControl/Services/ContractService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContractControl.Entities;
+
+namespace ContractControl.Services
+{
+    internal class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+            for(int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updateQuota = basicQuota + _onlinePaymentService.Interest(basicQuota, i);
+                double fullQuota = updateQuota + _onlinePaymentService.PaymentFee(updateQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+
+            }
+        }
+    }
+}
